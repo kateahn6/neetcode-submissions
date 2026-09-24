82c5215e42873c9c87a5686a7aa556b4bb41d6ff
Kate Ahn <kate.ahn2006@gmail.com>
2026-09-23 23:30:35 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        # Num. of char in both strings 
+        countS, countT = {} , {}
+
+        for i in range(len(s)):
+            # Count occurence of each character in the string
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        for c in countS:
+            if countS[c] != countT.get(c,0):
+                return False
+        return True    
+
+        
